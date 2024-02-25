e8f1206fd419d34e10b8ce39262a618359135b15
kkssuu <155282573+kkssuu@users.noreply.github.com>
2024-02-25 13:48:13 +0900

    Update script_bank.py

--- src/script_bank.py
+++ src/script_bank.py
@@ -1,1 +1,17 @@
+lb_cardname=Element("lb_cardname")
+lb_hello=Element("lb_hello")
 
+def loadAll(*args):
+    f=open("db.txt",'r')
+
+    name=f.readline()
+    
+    f.close()
+
+    lb_hello.element.innerText= name+'님 안녕하세요'
+    lb_cardname.element.innerText= name
+    
+
+# .txt 들어가기 쉬움
+# .ini(initialize:초기화하다) txt와 동일 (windows) 설정 초기화 
+# .bin(binary:2진수) 잘안열림
